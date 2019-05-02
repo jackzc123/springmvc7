ed63c0d8cf652daa8b3b73ade0b525679797c584
jackzc123 <zc1687675694@163.com>
2019-05-02 15:17:20 +0800

    文件上传功能

--- src/main/java/com/usoft/controller/i18nController.java
+++ src/main/java/com/usoft/controller/i18nController.java
@@ -3,6 +3,11 @@ package com.usoft.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * @author zc
@@ -11,8 +16,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class i18nController {
 
+    /**
+     * i18n国际化测试
+     * @return
+     */
     @RequestMapping(value = "/i18n", method = RequestMethod.GET)
     public String i18ntest(){
+        return "i18n";
+    }
+
+    @RequestMapping(value = "/fileUp", method = RequestMethod.POST)
+    public String fileUploade(@RequestParam("file")MultipartFile file, @RequestParam("desc")String desc) throws IOException {
+        if (!file.isEmpty()){
+            System.out.println(desc);
+            String path = "E:\\IdeaProjects\\springmvc7\\src\\main\\resources\\";
+            String name = file.getOriginalFilename();
+            File file1 = new File(path , name);
+            file.transferTo(file1);
+        }
         return "i18n";
     }
 
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,8 +1,15 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <body>
 <h2>Hello World!</h2>
 
 <a href="/i18n">i18n</a>
+
+<form action="/fileUp" method="post" enctype="multipart/form-data">
+    fileName:<input type="file" name="file"/><br>
+    fileDesc:<input type="text" name="desc"/><br>
+    <input type="submit" value="提交"/>
+</form>
 
 </body>
 </html>
